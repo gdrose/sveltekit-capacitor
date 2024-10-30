4b906f91d44ed9df8159201965fdeaacaa26ec93
Giorgio Di Rosa <63345060+gdrose@users.noreply.github.com>
2024-10-30 11:47:59 +0100

    Capacitor + Sveltekit init

---
+++ android/app/src/main/java/com/tap2ai/cap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tap2ai.cap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.tap2ai.cap',
+	appName: 'tap2cap',
+	webDir: 'build'
+};
+
+export default config;
