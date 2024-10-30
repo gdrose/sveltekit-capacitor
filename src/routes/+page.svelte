5265f23977516ec539b8dabd7d4332a1059d8244
Giorgio Di Rosa <63345060+gdrose@users.noreply.github.com>
2024-10-30 12:16:03 +0100

    Adding GoogleService-Info + updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang="ts">
+	import { FirebaseFirestore } from '@capacitor-firebase/firestore';
+
+	const addDocument = async () => {
+		await FirebaseFirestore.addDocument({
+			reference: 'users',
+			data: {
+				first: 'Alan',
+				last: 'Turing',
+				born: 1912
+			}
+		});
+	};
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button on:click={addDocument}>Add User Document</button>
